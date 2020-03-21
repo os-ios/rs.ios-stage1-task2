d3f480f31a950d71be83040104f51670563c5d59
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-03-22 00:10:22 +0300

    fixed error in test

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,6 +2,18 @@
 
 @implementation Blocks
 
+- (instancetype)init {
+
+    self.blockA = ^(NSArray *array) {
+        NSLog(@"got A");
+    };
+
+    self.blockB = ^(Class clazz) {
+        NSLog(@"got B");
+    };
+
+    return self;
+}
 
 @end
 

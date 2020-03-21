4ec7927d626268982da156241d3b1f65ccfb0cfd
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-03-22 01:28:41 +0300

    task 1 completed

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,25 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSString *result;
+    
+    // convert (day, month, year) to NSString
+    NSString *inputString = [NSString stringWithFormat:@"%2@-%2@-%4@", day, month, year];
+    
+    // build NSDate from inputString
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"dd-MM-yyyy"; //  input format
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    NSDate *date = [dateFormatter dateFromString: inputString];
+
+    if (date == nil) {
+        result = @"Такого дня не существует";
+    } else {
+        dateFormatter.dateFormat = @"dd MMMM, EEEE"; // output format
+        result = [dateFormatter stringFromDate:date];
+    }
+    
+    return result;
 }
 
 @end

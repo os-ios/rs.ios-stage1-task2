959c8937ceb594cc640923cae7ba25ce1ec5e036
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-03-23 10:41:34 +0300

    task 6 completed

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,18 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
+    NSMutableArray<NSNumber *> *arr = [[NSMutableArray alloc] initWithCapacity:number.intValue];
+    [arr addObject:@0];
+    [arr addObject:@1];
+
+    for (int i = 2; i <= number.intValue; i++) {
+        arr[i] = @(arr[i - 2].intValue + arr[i - 1].intValue);
+        if (arr[i - 2].intValue * arr[i - 1].intValue >= number.intValue) {
+            return @[arr[i - 2], arr[i - 1], arr[i - 2].intValue * arr[i - 1].intValue == number.intValue ? @1 : @0];
+        }
+    }
+
     return @[];
 }
+
 @end

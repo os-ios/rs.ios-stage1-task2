f4839a225e0f1da810652fc1b974f5be302d61b9
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-03-23 22:48:24 +0300

    task 2 code refactoring

--- RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
+++ RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
@@ -1,17 +1,26 @@
 #import "TimeConverter.h"
 
 @implementation TimeConverter
+
+static NSArray<NSString *> *nums;
+
+- (instancetype)init {
+    self = [super init];
+    if (self != nil) {
+        nums = @[@"zero", @"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten", @"eleven", @"twelve", @"thirteen", @"fourteen", @"fifteen", @"sixteen", @"seventeen", @"eighteen", @"nineteen", @"twenty", @"twenty one", @"twenty two", @"twenty three", @"twenty four", @"twenty five", @"twenty six", @"twenty seven", @"twenty eight", @"twenty nine"];
+    }
+    return self;
+}
+
 // Complete the following function
-- (NSString*)convertFromHours:(NSString*)hours minutes:(NSString*)minutes {
+- (NSString *)convertFromHours:(NSString *)hours minutes:(NSString *)minutes {
     int h = [hours intValue];
     int m = [minutes intValue];
-    
+
     if (h < 0 || h > 24 || m < 0 || m > 60) {
         return @"";
     }
-    
-    NSArray<NSString *> *nums = @[@"zero", @"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten", @"eleven", @"twelve", @"thirteen", @"fourteen", @"fifteen", @"sixteen", @"seventeen", @"eighteen", @"nineteen", @"twenty", @"twenty one", @"twenty two", @"twenty three", @"twenty four", @"twenty five", @"twenty six", @"twenty seven", @"twenty eight", @"twenty nine"];
-    
+
     if (m == 0)
         return [NSString stringWithFormat:@"%@%@", nums[h], @" o' clock"];
     else if (m == 1)
@@ -25,9 +34,9 @@
     else if (m == 45)
         return [NSString stringWithFormat:@"%@%@", @"quarter to ", nums[(h % 12) + 1]];
     else if (m <= 30)
-        return [NSString stringWithFormat:@"%@%@%@", nums[m], @" minutes past ",  nums[h]];
+        return [NSString stringWithFormat:@"%@%@%@", nums[m], @" minutes past ", nums[h]];
     else if (m > 30)
-        return [NSString stringWithFormat:@"%@%@%@", nums[60 - m],  @" minutes to ", nums[(h % 12) + 1]];
+        return [NSString stringWithFormat:@"%@%@%@", nums[60 - m], @" minutes to ", nums[(h % 12) + 1]];
     return @"";
 }
 @end

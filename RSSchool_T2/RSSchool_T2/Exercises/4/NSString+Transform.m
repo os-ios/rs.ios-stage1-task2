27cf04ac368a7327331c5d9e76eabba3947c115f
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2020-03-23 22:37:49 +0300

    task 4 code refactoring

--- RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
+++ RSSchool_T2/RSSchool_T2/Exercises/4/NSString+Transform.m
@@ -2,7 +2,7 @@
 
 @implementation NSString (Transform)
 
--(NSString*)transform {
+- (NSString *)transform {
     NSArray<NSString *> *alphabet = @[@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
     NSArray<NSString *> *vowels = @[@"a", @"A", @"e", @"E", @"i", @"I", @"o", @"O", @"u", @"U", @"y", @"Y"];
     NSArray<NSString *> *consonants = @[@"b", @"B", @"c", @"C", @"d", @"D", @"f", @"F", @"g", @"G", @"h", @"H", @"j", @"J", @"k", @"K", @"l", @"L", @"m", @"M", @"n", @"N", @"p", @"P", @"q", @"Q", @"r", @"R", @"s", @"S", @"t", @"T", @"v", @"V", @"w", @"W", @"x", @"X", @"z", @"Z"];
@@ -16,46 +16,29 @@
     }
 
     NSArray<NSString *> *words = [[self stringByReplacingOccurrencesOfString:@"\n" withString:@""] componentsSeparatedByString:@" "];
-    if (pangram) {
-        for (int i = 0; i < words.count; i++) {
-            NSMutableString *sentence = [words[i] mutableCopy];
+    for (int i = 0; i < words.count; i++) {
+        NSMutableString *word = [words[i] mutableCopy];
 
-            if (sentence.length != 0) {
-                int count = 0;
-                for (int j = 0; j < sentence.length; j++) {
-                    NSString *character = [sentence substringWithRange:NSMakeRange(j, 1)];
-                    if ([vowels containsObject:character]) { // TODO: convert to Block
-                        count++;
-                        [sentence replaceCharactersInRange:NSMakeRange(j, 1) withString:[character uppercaseString]];
-                    }
+        if (word.length != 0) {
+            int count = 0;
+            for (int j = 0; j < word.length; j++) {
+                NSString *character = [word substringWithRange:NSMakeRange(j, 1)];
+
+                BOOL vowelOrConsonant = pangram ? [vowels containsObject:character] : [consonants containsObject:character];
+                if (vowelOrConsonant) {
+                    count++;
+                    [word replaceCharactersInRange:NSMakeRange(j, 1) withString:[character uppercaseString]];
                 }
-
-                [result addObject:[NSString stringWithFormat:@"%d%@", count, sentence]];
             }
-        }
-    } else {
-        for (int i = 0; i < words.count; i++) {
-            NSMutableString *word = [words[i] mutableCopy];
 
-            if (word.length != 0) {
-                int count = 0;
-                for (int j = 0; j < word.length; j++) {
-                    NSString *character = [word substringWithRange:NSMakeRange(j, 1)];
-                    if ([consonants containsObject:character]) { // TODO: convert to Block
-                        count++;
-                        [word replaceCharactersInRange:NSMakeRange(j, 1) withString:[character uppercaseString]];
-                    }
-                }
-
-                [result addObject:[NSString stringWithFormat:@"%d%@", count, word]];
-            }
+            [result addObject:[NSString stringWithFormat:@"%d%@", count, word]];
         }
     }
 
-    NSArray *array = [result sortedArrayUsingComparator:^(NSString *sentence1, NSString *sentence2) {
-        NSString *count1 = [sentence1 substringWithRange:NSMakeRange(0, 1)];
-        NSString *count2 = [sentence2 substringWithRange:NSMakeRange(0, 1)];
-        return [count1 compare: count2];
+    NSArray *array = [result sortedArrayUsingComparator:^(NSString *word1, NSString *word2) {
+        NSString *word1Count = [word1 substringWithRange:NSMakeRange(0, 1)];
+        NSString *word2Count = [word2 substringWithRange:NSMakeRange(0, 1)];
+        return [word1Count compare:word2Count];
     }];
 
     NSMutableString *resultString = [NSMutableString new];
